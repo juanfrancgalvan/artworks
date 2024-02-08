75446ae9b6ed80d54da9effb9e49cd0f139b8c7d
Juan Francisco <juanfrancgalvan@gmail.com>
2024-02-08 16:05:27 -0300

    modified:   src/script.js

--- src/script.js
+++ src/script.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const galleryImages = document.querySelectorAll('img');
   const modal = document.getElementById('modal');
   const expandedImage = document.getElementById('expand-image');
   const closeButton = document.getElementById('close');
 
   galleryImages.forEach(img => {
-      img.addEventListener('click', function() {
-          modal.style.display = 'block';
-          expandedImage.src = this.src;
-      });
+    img.addEventListener('click', function () {
+      modal.style.display = 'block';
+      expandedImage.src = this.src;
+    });
   });
 
-  closeButton.addEventListener('click', function() {
-      modal.style.display = 'none';
+  closeButton.addEventListener('click', function () {
+    modal.style.display = 'none';
   });
 });
