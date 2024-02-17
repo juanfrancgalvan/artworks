d601d99499a7d152c25038807ced93186bf78975
Juan Francisco <juanfrancgalvan@gmail.com>
2024-02-17 19:00:06 -0300

    renamed:    build/images/portada1.webp -> build/images/cover1.webp 	renamed:    build/images/portada2.webp -> build/images/cover2.webp 	renamed:    build/images/portada3.webp -> build/images/cover3.webp 	modified:   build/style.css 	modified:   build/style.css.map 	modified:   index.html 	renamed:    src/images/portada1.jpg -> src/images/cover1.jpg 	renamed:    src/images/portada2.jpg -> src/images/cover2.jpg 	renamed:    src/images/portada3.jpg -> src/images/cover3.jpg 	modified:   src/script.js 	modified:   src/style.scss

--- src/script.js
+++ src/script.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const imagenes = document.querySelectorAll('img');
+  const images = document.querySelectorAll('img');
   const modal = document.querySelector('#modal');
-  const expandirImagen = document.querySelector('#expandir-imagen');
-  const botonCerrar = document.querySelector('#cerrar');
+  const expandImage = document.querySelector('#expand-image');
+  const closeButton = document.querySelector('#close');
 
-  imagenes.forEach(img => {
+  images.forEach(img => {
     img.addEventListener('click', function () {
       modal.style.display = 'block';
-      expandirImagen.src = this.src;
+      expandImage.src = this.src;
     });
   });
 
-  botonCerrar.addEventListener('click', function () {
+  closeButton.addEventListener('click', function () {
     modal.style.display = 'none';
   });
 });
