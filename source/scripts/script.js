388d549f8c098572bc8213703f1a929dbb3f1fc5
Juan Francisco <juanfrancgalvan@gmail.com>
2024-03-02 17:59:07 -0300

    modified:   build/scripts/script.js 	modified:   build/scripts/script.js.map 	modified:   build/styles/style.css 	modified:   build/styles/style.css.map 	modified:   source/scripts/script.js 	modified:   source/styles/layout/covers.scss

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -34,6 +34,8 @@ const covers = [
 ];
 
 function createCovers() {
+
+  // Create covers
   const main = document.querySelector('main');
   covers.forEach(cover => {
     const section = document.createElement('section');
@@ -43,8 +45,25 @@ function createCovers() {
       <p>Lorem ipsum dolor sit</p>
       <img src="${cover.cover}" alt="${cover.name} cover">
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-    `;
+      `;
     main.append(section);
+
+    // Show image
+    section.addEventListener('click', function showImage() {
+      const body = document.querySelector('body')
+      const overlay = document.createElement('div')
+      overlay.classList.add('overlay')
+      const image = document.createElement('img')
+      image.src = cover.cover;
+      image.alt = `${cover.name} cover`
+      overlay.appendChild(image)
+      body.appendChild(overlay)
+      
+      // Close image
+      image.addEventListener('click', function closeImage() {
+        overlay.remove()
+      })
+    })
   });
 }
 createCovers();
